eec7c012340ecebf8c20cfc929f5e23e7aa0590c
TULIKA DAS <159255355+tulika105@users.noreply.github.com>
2024-08-02 22:59:14 +0530

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+from PIL import Image
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load the trained model and vectorizer
+model = joblib.load('sentiment_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+# Function to predict sentiment
+def predict_sentiment(text):
+    text_transformed = vectorizer.transform([text])
+    prediction = model.predict(text_transformed)
+    return prediction[0]
+
+# Streamlit app layout
+st.title('Student Sentiment Analysis App ')
+image_path = "C:/Users/91981/Pictures/Screenshots/Screenshot 2024-04-10 034613.png"
+image = Image.open(image_path)
+st.image(image, use_column_width=True)
+
+st.write('Enter a sentence to get its sentiment prediction.')
+
+text_input = st.text_area('Input Text')
+
+if st.button('Predict'):
+    if text_input:
+        sentiment = predict_sentiment(text_input)
+        st.write(f'Sentiment: {sentiment}')
+    else:
+        st.write('Please enter some text.')
