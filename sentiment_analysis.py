34533d4230b30ce474fe990ce67ae4cd729423af
TULIKA DAS <159255355+tulika105@users.noreply.github.com>
2024-08-04 12:54:47 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import os
 import joblib
 from PIL import Image
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -17,10 +16,6 @@ def predict_sentiment(text):
 # Streamlit app layout
 st.title('Student Sentiment Analysis App ')
 image_path = "C:/Users/91981/sentiment image.png"
-if not os.path.isfile(image_path):
-    print(f"Error: The file {image_path} does not exist.")
-else:
-    image = Image.open(image_path)
 image = Image.open(image_path)
 st.image(image, use_column_width=True)
 
