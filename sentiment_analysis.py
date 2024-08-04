db68994a92876a36a73976a24f52344341bb9c2c
TULIKA DAS <159255355+tulika105@users.noreply.github.com>
2024-08-04 14:06:40 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -15,7 +15,7 @@ def predict_sentiment(text):
 
 # Streamlit app layout
 st.title('Student Sentiment Analysis App ')
-image_path = "C:\Users\91981\sentiment image.png"
+image_path = r"C:\Users\91981\sentiment image.png"
 st.image(image_path, use_column_width=True)
 
 st.write('Enter a sentence to get its sentiment prediction.')
